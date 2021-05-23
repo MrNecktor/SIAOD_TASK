0569e3b2ec398660a7e72cb12be84e4d3f7e5a6a
MrNecktor <67980242+MrNecktor@users.noreply.github.com>
2021-05-23 21:08:28 +0300

    Add files via upload

---
+++ SIAOD_TAS_COIN/cmake-build-debug/CMakeFiles/SIAOD_TAS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SIAOD_TAS.dir/main.cpp.o"
+  "SIAOD_TAS"
+  "SIAOD_TAS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SIAOD_TAS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
